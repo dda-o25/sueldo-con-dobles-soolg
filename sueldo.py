f87b6c21f704b9ba923cc63a185b3d4213e95e32
soolg <sol.gonzalez@iteso.mx>
2025-09-19 00:42:42 -0600

    primer intento

--- sueldo.py
+++ sueldo.py
@@ -1,16 +1,25 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Sol Valeria Gonzalez Castro
+19/09/2025
+Calcular el sueldo semanal de un empleado, dadas las horas trabajadas y la tarifa horaria
 """
 
-
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+horas_trabajadas = float(input("Introduzca horas trabajadas : "))
+tarifa_horaria = float(input("Introduzca la tarifa: "))
 
 # Proceso
-
+if horas_trabajadas > 48:
+    horas_extra = horas_trabajadas - 48
+    pago_extra = horas_extra * tarifa_horaria * 2
+    pago = 48 * tarifa_horaria
+else:
+    horas_extra = 0
+    pago_extra = 0
+    pago = horas_trabajadas * tarifa_horaria
+pago_total = pago + pago_extra
 
 # Salidas
-print(salida)
+print("Horas extra:", horas_extra)
+print("Pago extra:", pago_extra)
+print("Total:", pago_total)
